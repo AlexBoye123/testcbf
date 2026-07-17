4167f86f6c348e03967053ce0a559304d74dc969
AlexBoye123 <112716872+AlexBoye123@users.noreply.github.com>
2026-07-17 23:57:39 +0300

    Add files via upload

---
+++ src/JFPUI.cpp
@@ -0,0 +1,51 @@
+#include "JFPUI.hpp"
+#include "CorridorGenerator.hpp"
+#include <Geode/modify/CreatorLayer.hpp>
+
+using namespace geode::prelude;
+
+namespace JFP {
+
+void createWavemanLevel() {
+    auto level = GJGameLevel::create();
+    if (!level) return;
+
+    level->m_levelName = "JFP Random Corridor";
+    level->m_levelString = CorridorGenerator::get().generateLevelString(250);
+    level->m_levelType = GJLevelType::Editor;
+
+    auto scene = PlayLayer::scene(level, false, false);
+    CCDirector::sharedDirector()->replaceScene(scene);
+}
+
+} // namespace JFP
+
+class $modify(JFPMenuHook, CreatorLayer) {
+    bool init() {
+        if (!CreatorLayer::init()) return false;
+
+        auto menu = this->getChildByID("creator-buttons-menu");
+        if (!menu) {
+            menu = m_bottomMenu;
+        }
+
+        if (menu) {
+            auto spr = CCSprite::createWithSpriteFrameName("GJ_playBtn2_001.png");
+            if (spr) {
+                spr->setScale(0.6f);
+                auto btn = CCMenuItemSpriteExtra::create(
+                    spr, this, menu_selector(JFPMenuHook::onJFPButton)
+                );
+                btn->setID("jfp-button");
+                menu->addChild(btn);
+                menu->updateLayout();
+            }
+        }
+
+        return true;
+    }
+
+    void onJFPButton(CCObject* sender) {
+        JFP::createWavemanLevel();
+    }
+};
---
+++ src/JFPUI.hpp
@@ -0,0 +1,12 @@
+#ifndef JFP_UI_HPP
+#define JFP_UI_HPP
+
+#include <Geode/Geode.hpp>
+
+namespace JFP {
+
+void createWavemanLevel();
+
+} // namespace JFP
+
+#endif // JFP_UI_HPP
