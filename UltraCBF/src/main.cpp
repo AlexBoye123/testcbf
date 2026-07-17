c13147fb777d1072cba48b00daac10d6d7ed6264
AlexBoye123 <112716872+AlexBoye123@users.noreply.github.com>
2026-07-17 22:16:07 +0300

    Update main.cpp

--- UltraCBF/src/main.cpp
+++ UltraCBF/src/main.cpp
@@ -2,6 +2,13 @@
 #include "SubTickEngine.hpp"
 #include "Hooks.hpp"
 
+#ifdef GEODE_IS_WINDOWS
+#include <windows.h>
+#include <timeapi.h>
+
+typedef NTSTATUS(NTAPI* pfnNtSetTimerResolution)(ULONG DesiredResolution, BOOLEAN SetResolution, PULONG ActualResolution);
+#endif
+
 using namespace geode::prelude;
 
 $on_mod(Loaded) {
@@ -10,6 +17,26 @@ $on_mod(Loaded) {
     log::info(" Target: Geometry Dash 2.2081 | Geode SDK v5.8.2 ");
     log::info(" Lock-Free SPSC Ring Buffer + Real-Time Profiler ");
     log::info("=================================================");
+
+#ifdef GEODE_IS_WINDOWS
+    // 1. Set Win32 Multimedia timer to 1ms minimum
+    timeBeginPeriod(1);
+
+    // 2. Elevate Windows NT Kernel Timer Resolution to 0.5ms (500 microseconds / 5000 100-ns units)
+    HMODULE hNtdll = GetModuleHandleA("ntdll.dll");
+    if (hNtdll) {
+        auto NtSetTimerResolution = reinterpret_cast<pfnNtSetTimerResolution>(GetProcAddress(hNtdll, "NtSetTimerResolution"));
+        if (NtSetTimerResolution) {
+            ULONG actualRes = 0;
+            NtSetTimerResolution(5000, TRUE, &actualRes);
+            log::info("[UltraCBF] Windows NT Kernel Timer Resolution boosted to 0.5ms (500us)!");
+        }
+    }
+
+    // 3. Elevate current thread priority to ensure zero-latency context switching
+    SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_HIGHEST);
+    log::info("[UltraCBF] Windows high-precision kernel timers & highest thread priority engaged.");
+#endif
 
     // Calibrate QueryPerformanceCounter hardware frequency
     UltraCBF::SubTickEngine::get().init();
